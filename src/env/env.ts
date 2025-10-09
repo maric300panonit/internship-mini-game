167d39bee5a753c13ff1643d1643fbe11afbdd08
Branko Maric <branko.maric@panonit.com>
2025-10-09 16:30:24 +0200

    added env.ts with global variables

---
+++ src/env/env.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    assetsPath: "assets/",
+    ground_level: 400,
+    lastClickTime: 0,
+    doubleClickThreshold: 300, // milliseconds,
+    isDoubleClickActive: false
+}
